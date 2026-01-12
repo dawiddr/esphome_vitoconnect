bc30d6f1449fb29e98ca5aa05003d7082cca3694
Stefan Bickel <info@stefanbickel.com>
2026-01-12 18:21:52 +0100

    fix(vitoconnect): remove protocol specific verification step

--- components/vitoconnect/vitoconnect.cpp
+++ components/vitoconnect/vitoconnect.cpp
@@ -84,30 +84,16 @@ void VitoConnect::update() {
       // write the modified datapoint
       CbArg* writeCbArg = new CbArg(this, dp, true, dp->getLastUpdate());
       if (!_optolink->write(dp->getAddress(), dp->getLength(), data, reinterpret_cast<void*>(writeCbArg))) {
+        ESP_LOGD(TAG, "Write operation for datapoint with address %x has been completed", writeCbArg->dp->getAddress());
+        writeCbArg->dp->clearLastUpdate(); // clear dirty flag
         delete writeCbArg;
-        return;
-      }
-
-      // read the same datapoint to verify the previous write
-      uint8_t* readCbData = new uint8_t[dp->getLength()]; // readCbArg has to own this memory
-      memcpy(readCbData, data, dp->getLength());
-
-      CbArg* readCbArg = new CbArg(this, dp, false, 0, readCbData);
-      if (!_optolink->read(dp->getAddress(), dp->getLength(), reinterpret_cast<void*>(readCbArg))) {
-        delete readCbArg;
-        delete[] readCbData;
         return;
       }
     }
   }
 
-  if(foundDirty) {
-    ESP_LOGD(TAG, "Found dirty datapoint(s), skip polling cycle.");
-    return;
-  }
-
   for (Datapoint* dp : this->_datapoints) {
-      CbArg* arg = new CbArg(this, dp, false, 0);
+      CbArg* arg = new CbArg(this, dp);
       if (_optolink->read(dp->getAddress(), dp->getLength(), reinterpret_cast<void*>(arg))) {
       } else {
           delete arg;
@@ -118,26 +104,12 @@ void VitoConnect::update() {
 void VitoConnect::_onData(uint8_t* data, uint8_t len, void* arg) {
   CbArg* cbArg = reinterpret_cast<CbArg*>(arg);
 
-  if (cbArg->dp->getLastUpdate() > 0) {
-    if (!cbArg->w && cbArg->d == nullptr) {
-      ESP_LOGD(TAG, "Datapoint with address %x is eventually being written, waiting for confirmation.", cbArg->dp->getAddress());
-    } else if (cbArg->w) { // this was a write operation
-      ESP_LOGD(TAG, "Write operation for datapoint with address %x %s.",
-             cbArg->dp->getAddress(), data[0] == 0x00 ? "has been completed" : "failed");
-    } else if (cbArg->d != nullptr) { // cbArg->d is only set if this read is intended to verify a previous write
-      ESP_LOGD(TAG, "Verifying received data for datapoint with address %x.", cbArg->dp->getAddress());
-
-      if (len != cbArg->dp->getLength()) {
-        ESP_LOGW(TAG, "Expected length of %d was not met for datapoint with address %x.", cbArg->dp->getLength(), cbArg->dp->getAddress());
-      } else if (memcmp(data, cbArg->d, len) == 0) {
-        ESP_LOGD(TAG, "Previous write operation for datapoint with address %x was successfully verified.", cbArg->dp->getAddress());
-        cbArg->dp->clearLastUpdate();
-      } else {
-        ESP_LOGW(TAG, "Previous write operation for datapoint with address %x failed verification.", cbArg->dp->getAddress());
-      }
+  if (!cbArg->w) { // ignore onData responses for writes
+    if (cbArg->dp->getLastUpdate() > 0) {
+      ESP_LOGD(TAG, "Datapoint with address %x is being written, ignoring read responses until completion.", cbArg->dp->getAddress());
+    } else {
+      cbArg->dp->decode(data, len, cbArg->dp);
     }
-  } else if (!cbArg->w) {
-    cbArg->dp->decode(data, len, cbArg->dp);
   }
 
   delete cbArg;
